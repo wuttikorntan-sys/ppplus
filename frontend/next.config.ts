5c3a31e4873f7e8e72d26859e7c45093e3026c5b
PPPLUS <admin@ppplus.co.th>
2026-04-07 07:38:55 +0700

    fix: add unoptimized:true to images config - fixes all Image display in standalone mode

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -70,6 +70,7 @@ const nextConfig: NextConfig = {
     },
   },
   images: {
+    unoptimized: true,
     dangerouslyAllowSVG: true,
     contentDispositionType: "attachment",
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
